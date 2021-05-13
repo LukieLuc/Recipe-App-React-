ed841097c5c89e346c53a07003ca6d254b32cb44
LukieLuc <82279596+LukieLuc@users.noreply.github.com>
2021-05-13 16:56:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Recipe from "./Recipe";
 import "./App.css";
 import "./index.css";
-import logo from "green-recipe.png";
 
 const App = () => {
 	const APP_ID = "c78cf46f";
